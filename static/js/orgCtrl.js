2dd9d4ff830f523bbb8ff2f1f4a7ab952ef1e6bb
Christopher Merrick <cmerrick@rjmetrics.com>
2013-08-04 16:05:00 -0400

    better event summary

--- static/js/orgCtrl.js
+++ static/js/orgCtrl.js
@@ -27,7 +27,6 @@ var app = angular.module('gitmap', ['ui.bootstrap'])
 
 function OrgCtrl($scope, $http, $routeParams) {
     $scope.milestones = [];
-    $scope.repo_events = [];
     $scope.orgname = $routeParams.orgname;
     
     $http({
@@ -49,19 +48,48 @@ function OrgCtrl($scope, $http, $routeParams) {
 		method: 'GET',
 		url: "/api/repos/" + $scope.orgname + "/" + repo + "/issues/events.json"
 	    }).success(function(data, status) {
-		_(data).each(function(event) {
-		    if(event.issue && event.issue.milestone) {
+		_(data).chain()
+                    .filter(function(e) { return e.issue && e.issue.milestone; })
+                    .each(function(event) {
 			milestone = _($scope.milestones).find(
 			    function(milestone) {
 				return milestone.repo.name == repo && milestone.number == event.issue.milestone.number;
 			    });
-			if(!milestone.events) milestone.events = []
-			milestone.events.push(event);
-		    }
-		});
-	    });		    
-	});
-    }
+			if(!milestone.eventsByIssue) 
+                            milestone.eventsByIssue = [];
+                        if(!milestone.eventsByIssue[event.issue.number])
+                            milestone.eventsByIssue[event.issue.number] = [];
+
+			milestone.eventsByIssue[event.issue.number].push(event);
+		    });
+
+                _.chain($scope.milestones)
+                    .filter(function(m) { return m.repo.name == repo; })
+                    .each(function(m) { 
+                        m.event_summaries = summarizeEvents(m.eventsByIssue);
+                        return m;
+                    });
+	    });
+            
+	});		    
+    };
+
+    var summarizeEvents = function(eventsByIssue) {        
+        return _(eventsByIssue).map(function(issueEvents) { 
+            var out = {};
+            out.title = _(issueEvents).first().issue.title;
+            out.url = _(issueEvents).first().issue.html_url;
+            out.last_modified = _(issueEvents).max(function(event) {
+                return moment(event.created_at);
+            }).created_at;
+
+            out.users = _.chain(issueEvents)
+                .map(function(e) { return e.actor })
+                .uniq(function(u){ return u.login; })
+                .value();
+            return out;
+        });
+    };
 
     $scope.progress = function(milestone) {
 	return !milestone.open_issues ? milestone.open_issues : 
